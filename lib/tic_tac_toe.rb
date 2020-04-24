c8030178d654f8e6858d1a965f315366348f5ea5
Samir Triande <habib.triande@gmail.com>
2020-04-24 16:24:03 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -36,7 +36,11 @@ class TicTacToe
 
 
   def valid_move?(board, index)
-    index.between?(0,8) && !position_taken?(@board, index)
+    if index.between?(0,8) && !position_taken?(@board, index)
+      true
+    else
+      false
+    end
   end
 
   def turn(board)
