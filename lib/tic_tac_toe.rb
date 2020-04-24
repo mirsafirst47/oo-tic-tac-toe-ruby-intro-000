e7a8a0e74a09101758796abd3a25c00f240f55be
Samir Triande <habib.triande@gmail.com>
2020-04-24 16:58:06 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -110,7 +110,7 @@ class TicTacToe
       turn
     end
     if won?
-      puts "Congratulations #{winner(@board)}!"
+      puts "Congratulations #{winner}!"
     elsif draw?
       puts "Cat's Game!"
     end
